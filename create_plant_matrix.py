99476a63dd2c4bf8fe8ddcfbe5e368bb5383f836
JohnWes7 <1246215651@qq.com>
2025-10-13 17:33:13 +0800

    修复了一些游戏不能读取的buug

--- create_plant_matrix.py
+++ create_plant_matrix.py
@@ -1,30 +1,45 @@
-# 创建二维植物矩阵，维度与世界大小相同
-plant_matrix = []
+def create_plant_matrix():	
 
-# 遍历Y轴坐标
-for y in range(get_world_size()):
-    row_list = []  # 当前行的植物列表
+	# 创建二维植物矩阵，维度与世界大小相同
+	plant_matrix = []
 
-    # 遍历X轴坐标
-    for x in range(get_world_size()):
+	# 遍历Y轴坐标
+	for y in range(get_world_size()):
+		row_list = []  # 当前行的植物列表
 
-        # 下半部分区域（Y轴中点及以下）
-        if y >= get_world_size() // 2:
-            # 右下四分之一区域种植南瓜
-            if x >= get_world_size() // 2:
-                row_list.append(Entities.Pumpkin)
-            # 左下四分之一区域种植胡萝卜
-            else:
-                row_list.append(Entities.Carrot)
+		# 遍历X轴坐标
+		for x in range(get_world_size()):
 
-        # 上半部分区域
-        else:
-            # 棋盘格模式交替种植树木和草地
-            if (x + y) % 2 == 0:
-                row_list.append(Entities.Tree)  # 偶数位置种植树木
-            else:
-                row_list.append(Entities.Grass)  # 奇数位置种植草地
+			# 下半部分区域（Y轴中点及以下）
+			if y >= get_world_size() // 2:
+				# 右下四分之一区域种植南瓜
+				if x >= get_world_size() // 2:
+					row_list.append(Entities.Pumpkin)
+				# 左下四分之一区域种植胡萝卜
+				else:
+					row_list.append(Entities.Carrot)
 
-    plant_matrix.append(row_list)
+			# 上半部分区域
+			else:
+				# 第一排种向日葵
+				if y == 0:
+					row_list.append(Entities.Sunflower)
 
-print(plant_matrix)
+				# 棋盘格模式交替种植树木和草地
+				if (x + y) % 2 == 0:
+					row_list.append(Entities.Tree)  # 偶数位置种植树木
+				else:
+					row_list.append(Entities.Grass)  # 奇数位置种植草地
+
+		plant_matrix.append(row_list)
+
+	return plant_matrix
+
+
+# def get_plant_matrix():
+#     plant_matrix
+#     # 全种向日葵
+#     for y in range(get_world_size()):
+#         for x in range(get_world_size()):
+#             if y == 0:
+#                 plant_matrix[y][x] = Entities.Sunflower
